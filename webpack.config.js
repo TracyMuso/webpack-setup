7572d42dc50c637fea8d835d9d830d9d33f0e293
Elfin Git <musongoletracy@gmail.com>
2022-08-09 22:24:54 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
